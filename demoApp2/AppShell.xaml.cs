c0df0d96808e93161e0dcb914db5d2bfdc5e290b
Grant Patterson <darkrider231@gmail.com>
2022-08-15 15:39:08 -0400

    Add project files.

---
+++ demoApp2/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using demoApp2.ViewModels;
+using demoApp2.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace demoApp2
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ demoApp2/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using demoApp2.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace demoApp2.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
